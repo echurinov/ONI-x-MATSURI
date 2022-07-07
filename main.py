da36d743565d8b5862e6371246abf9fe027e4588
rachel-west <rachel4zoe@gmail.com>
2022-07-07 17:04:59 +0900

    Updated sprites, added Start menu

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import random
 
 import arcade
 
-# From arcade online docs
 from gameManager import GameManager
 from screenView import GameView
 from screenView import StartView
@@ -11,12 +10,9 @@ from screenView import StartView
 def main():
     SCREEN_TITLE = "ONI x MATSURI"
 
-    # window = GameView(GameManager.SCREEN_WIDTH, GameManager.SCREEN_HEIGHT, SCREEN_TITLE)
     window = arcade.Window(GameManager.SCREEN_WIDTH, GameManager.SCREEN_HEIGHT, SCREEN_TITLE)
-    start_view = GameView()
+    start_view = StartView()
     window.show_view(start_view)
-    # window.setup()
-    start_view.setup()
     GameManager.start()
     window.run()
 
--- screenView.py
+++ screenView.py
@@ -2,6 +2,7 @@ import random
 
 import arcade
 
+# from arcade online docs
 from collider import Collider
 from entity import Entity
 from gameManager import GameManager
@@ -14,20 +15,23 @@ from transform import Transform
 
 class StartView(arcade.View):
     def on_show_view(self):
-        #This is run once when we switch to this view
-        arcade.set_background_color(arcade.csscolor.DARK_SLATE_BLUE)
+        """ This is run once when we switch to this view """
+        self.texture = arcade.load_texture("assets/backgrounds/start_screen.png")
 
         # Reset the viewport, necessary if we have a scrolling game and we need
         # to reset the viewport back to the start so we can see what we draw.
-        arcade.set_viewport(0, self.window.width, 0, self.window.height)
+        arcade.set_viewport(0, self.window.width - 1, 0, self.window.height - 1)
 
     def on_draw(self):
         # Draw this view
         self.clear()
-        arcade.draw_text("Instructions Screen", self.window.width / 2, self.window.height / 2,
-                         arcade.color.WHITE, font_size=50, anchor_x="center")
-        arcade.draw_text("Click to advance", self.window.width / 2, self.window.height / 2 - 75,
-                         arcade.color.WHITE, font_size=20, anchor_x="center")
+        self.texture.draw_sized(GameManager.SCREEN_WIDTH / 2, GameManager.SCREEN_HEIGHT / 2, GameManager.SCREEN_WIDTH, GameManager.SCREEN_HEIGHT)
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        """ If the user presses the mouse button, start the game. """
+        game_view = GameView()
+        game_view.setup()
+        self.window.show_view(game_view)
 
 
 class GameView(arcade.View):
