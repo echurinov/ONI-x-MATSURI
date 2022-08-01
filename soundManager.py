c2166e91828df7122667e8278ab4b04c9adbfde8
andymwat <andymwat@gmail.com>
2022-08-01 16:31:12 +0900

    Fix sound crashes

--- soundManager.py
+++ soundManager.py
@@ -19,31 +19,35 @@ class SoundManager:
     # __sound_lists will store the pre-loaded sounds
     # This class will create a pyglet.media.player.Player when play_sound() is invoked
     __sound_lists = {
-        "enemy_oni" : {
+        "enemy_oni": {
             "damage": arcade.load_sound(SOUND_PATH + "enemy/enemy_oni_damage_2.wav"),
             "death": arcade.load_sound(SOUND_PATH + "enemy/enemy_oni_death.wav")
         },
-        "enemy_oni_boss" : {
+        "enemy_oni_boss": {
 
         },
-        "player" : {
-            "attack" : arcade.load_sound(SOUND_PATH + "player/player_attack.wav"),
-            "damage" : arcade.load_sound(SOUND_PATH + "player/player_damage.wav"),
-            "jump" : arcade.load_sound(SOUND_PATH + "player/player_jump3.wav")
+        "player": {
+            "attack": arcade.load_sound(SOUND_PATH + "player/player_attack_16bit.wav"),
+            "damage": arcade.load_sound(SOUND_PATH + "player/player_damage.wav"),
+            "jump": arcade.load_sound(SOUND_PATH + "player/player_jump3_16bit.wav")
         },
-        "powerups" : {
-            "heal" : arcade.load_sound(SOUND_PATH + "powerups/cotton_candy.wav")
+        "powerups": {
+            "heal": arcade.load_sound(SOUND_PATH + "powerups/cotton_candy.wav")
         },
-        "user_interface" : {
-            "start_button_press" : arcade.load_sound(SOUND_PATH + "menu/button_press2.wav"),
-            "quit_button_press" : arcade.load_sound(SOUND_PATH + "menu/button_press.wav")
+        "user_interface": {
+            "start_button_press": arcade.load_sound(SOUND_PATH + "menu/button_press2.wav"),
+            "quit_button_press": arcade.load_sound(SOUND_PATH + "menu/button_press.wav")
         }
     }
 
     @staticmethod
-    def play_sound(domain: str, sound_name: str, sound_volume = SOUND_VOLUME):
+    def play_sound(domain: str, sound_name: str, sound_volume=SOUND_VOLUME):
         sound = SoundManager.__sound_lists[domain][sound_name]
         player = arcade.play_sound(sound, sound_volume)
+        # player object can be None if an error occurs (such as an unplayable sound)
+        if player is None:
+            print("Sound", domain + ":" + sound_name, "is not playable.")
+            player = pyglet.media.Player()  # Avoids crash later on
         sound_player = SoundPlayer(sound, player)
         SoundManager.__active_sounds.append(sound_player)
     
@@ -59,9 +63,6 @@ class SoundManager:
         if SoundManager.__active_sounds is None:
             return
         for sound_player in SoundManager.__active_sounds:
-            # Hacky workaround for crash. This should never be None but it is sometimes
-            if sound_player.player is None:
-                continue
             sound_player.sound.stop(sound_player.player)
 
     def __on_update(self):
@@ -70,9 +71,6 @@ class SoundManager:
         # Remove sounds that have finished playing
         list_copy = SoundManager.__active_sounds
         for sound_player in list_copy:
-            # Hacky workaround for crash
-            if sound_player.player is None:
-                continue
             if sound_player.sound.get_stream_position(sound_player.player) == 0.0:
                 SoundManager.__active_sounds.remove(sound_player)
 
