68467b3873ef196a7148b3ddb75e2b7f833313c5
rachel-west <rachel4zoe@gmail.com>
2022-08-03 15:49:07 +0900

    Fixed lose screen issues

--- screenView.py
+++ screenView.py
@@ -271,7 +271,6 @@ class GameView(arcade.View):
 
         GameManager.add_entity(boss_entity)
 
-
         # Reset and freeze camera
         GameManager.main_camera.move((0, 1080))
         arcade.set_viewport(0, self.window.width, 1080, self.window.height + 1080)
@@ -389,6 +388,7 @@ class LoseView(arcade.View):
         # Reset the viewport, necessary if we have a scrolling game and we need
         # to reset the viewport back to the start so we can see what we draw.
         GameManager.main_camera.move((0, 0))
+        arcade.set_viewport(0, self.window.width, 0, self.window.height)
 
     def on_draw(self):
         # Draw this view
